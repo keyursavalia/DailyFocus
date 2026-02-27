1cb65502f932409a9853883032e51a4f39eebe39
Keyur <savalia.keyur1091@gmail.com>
2026-02-26 22:03:57 -0800

    add: alertTitle case to handle empty task-title exception

--- Daily Focus/ViewModel/TaskError.swift
+++ Daily Focus/ViewModel/TaskError.swift
@@ -9,7 +9,16 @@ enum TaskError: LocalizedError, Equatable {
         case .limitReached:
             return "You can only focus on 3 things at once. Finish something first!"
         case .emptyTitle:
-            return "Task title cannot be empty"
+            return "Please enter a task name before adding."
+        }
+    }
+    
+    var alertTitle: String {
+        switch self {
+        case .limitReached:
+            return "Limit Reached"
+        case .emptyTitle:
+            return "Missing Title"
         }
     }
 }
